69c6acda84ed49ebe43d6fc26692225ee6cf96f0
Igor Santana do Nascimento <igorsantanan@hotmail.com>
2025-05-23 10:06:27 -0300

    ajustes na userClass

--- frontend/script/src/serverComunication.js
+++ frontend/script/src/serverComunication.js
@@ -3,15 +3,13 @@ let webSocket;
 export function serverBridge (msg_de_ida) {
     webSocket = new WebSocket('ws://localhost:4433');
     webSocket.onopen = () => {
-    webSocket.send(JSON.stringify(msg_de_ida));
+        webSocket.send(JSON.stringify(msg_de_ida));
 
-    webSocket.onmessage = ({ msg_de_volta} ) => {
-        let data = JSON.parse(msg_de_volta);
-        
+        webSocket.onmessage = ({ msg_de_volta} ) => {
+            let data = JSON.parse(msg_de_volta);
+            
 
-    }
-
-    webSocket.close();
+        }
     }
 }
 
--- frontend/script/src/userClass.js
+++ frontend/script/src/userClass.js
@@ -21,10 +21,22 @@ class User {
     }
 }
 
+let botao = document.getElementById("botaoCriarConta");
+botao.addEventListener("click", verificacaoCriarUsuario)
+
 function verificacaoCriarUsuario() {
     const nome = document.getElementById("nomeCompleto");
     const telefone = document.getElementById("telefone");
     const senha = document.getElementById("senha");
 
+    if ((nome.value != '' && nome.value.length >= 8) ) {
+        console.log("Nome Válido");
+    } else {
+        console.log("Nome Inválido");
+    }
 
+    console.log(nome.value);
+    console.log(telefone.value);
+    console.log(senha.value);
 }
+
